930ed56bad800f0aa99295cbed9b35f2c1e79b1b
Polina <chebanenkopolina@gmail.com>
2021-08-11 14:57:54 -0400

    loaded data from csv

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 
 app.get('/', (req, res) => res.json({ message: 'Hiiii' }));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
---
+++ server/models/questions.js
@@ -0,0 +1,8 @@
+const db = require('./../database');
+
+module.exports = {
+  getQuestionByProductId: (callback) => {
+    const query = 'here';
+    // how to send the query here?
+  },
+};
