0ed419c4de1561ff06bf5bdd09cc786327ec6137
Polina <chebanenkopolina@gmail.com>
2021-08-13 11:20:51 -0400

    initial data load complete

--- server/SCHEMA.sql
+++ server/SCHEMA.sql
@@ -1,31 +1,34 @@
-create schema questionsAndAnswers;
+DROP DATABASE IF EXISTS sdc;
+create database sdc;
+\c sdc
 
-create table questionsAndAnswers.questions (
+
+
+create table questions (
   question_id       serial primary key,
+  product_id        integer,
   body              text not null check (char_length(body) < 300),
-  created_at        timestamp,
+  created_at        text,
   score             integer,
   reported          integer,
-  user-id           integer REFERENCES users (user-id)
-);
-
-create table questionsAndAnswers.answers (
-  answer_id         serial primary key,
-  body              text not null check (char_length(body) < 300),
-  created_at        timestamp,
-  score             integer,
-  reported          integer,
-  user-id           integer REFERENCES users (user-id)
-);
-
-create table questionsAndAnswers.users (
-  user-id           serial primary key,
   username          text not null check (char_length(username) < 50),
-  email             text not null unique,
+  email             text not null
 );
 
-create table questionsAndAnswers.images (
+create table answers (
+  answer_id         serial primary key,
+  question_id       integer REFERENCES questions (question_id),
+  body              text not null check (char_length(body) < 300),
+  created_at        text,
+  score             integer,
+  reported          integer,
+  username          text not null check (char_length(username) < 50),
+  email             text not null
+);
+
+
+create table images (
   image_id           serial primary key,
   image_url          text not null,
-  answer_id          integer REFERENCES questions (answer_id)
+  answer_id          integer REFERENCES answers (answer_id)
 );
