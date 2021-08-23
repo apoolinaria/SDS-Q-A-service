583191797f7a12780a3fab4797bdebfbb35ca6cc
Polina <chebanenkopolina@gmail.com>
2021-08-23 16:07:44 -0400

    ready for deployment

--- server/database/index.js
+++ server/database/index.js
@@ -3,14 +3,15 @@ const { Client } = require('pg');
 const client = new Client({
   user: 'postgres',
   password: 'popo',
-  host: 'localhost',
-  port: '5432',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   database: 'sdc',
 });
 
 client
   .connect()
   .then(() => {
+    console.log(process.env.DB_HOST);
     console.log('Connected');
   })
   .catch((err) => {
--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 
-const port = 3001;
+const port = process.env.API_PORT || 3001;
+
 const router = require('./routes');
 app.use(express.json());
 app.use('/qa', router);
-
+app.get('/loaderio-67f67269d09940694a30b8462730e4d9/', (req, res) => {
+  res.send('loaderio-67f67269d09940694a30b8462730e4d9');
+});
 // app.get('/', (req, res) => res.json({ message: 'Hiiii' }));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
